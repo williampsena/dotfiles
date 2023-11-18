9f0c8c8e545f0b04620b473e0b032fac32463c6b
Will <williampsena@gmail.com>
2023-11-18 20:45:37 -0300

    update wallpaper

--- awesome/ebenezer/wallpaper.lua
+++ awesome/ebenezer/wallpaper.lua
@@ -4,39 +4,13 @@ local gears = require('gears')
 local envs = require('ebenezer.envs')
 local fs = require('ebenezer.helpers.fs')
 
-local function change_wallpaper(wallpaper, screen)
+local function change_wallpaper(screen, wallpaper)
     local wallpaper_path = fs.resolve_path(wallpaper, envs.path_vars)
-    gears.wallpaper.maximized(wallpaper_path, screen, true)
+    gears.wallpaper.maximized(wallpaper_path, s, true)
 end
 
-local function wallpaper_slideshow(screen)
-    local wallpapers = fs.get_files(envs.environment.wallpaper_dir)
-    local random_wallpaper = function()
-        return wallpapers[math.random(1, #wallpapers)]
-    end
-
-    if next(wallpapers) == nil then return end
-
-    change_wallpaper(random_wallpaper(), screen)
-
-    local wallpaper_timeout = envs.environment.wallpaper_timeout or 600
-    local wallpaper_timer = timer {timeout = wallpaper_timeout}
-
-    wallpaper_timer:connect_signal("timeout", function()
-        wallpaper_timer:stop() --       
-
-        change_wallpaper(random_wallpaper(), screen)
-
-        wallpaper_timer.timeout = wallpaper_timeout
-        wallpaper_timer:start()
-    end)
-
-    wallpaper_timer:start()
-end
-
-local function wallpaper_solo(screen)
-    local wallpaper =
-        fs.resolve_path(envs.environment.wallpaper, envs.path_vars)
+local function setup_wallpaper(screen, wallpaper)
+    local wallpaper = fs.resolve_path(wallpaper, envs.path_vars)
 
     screen.connect_signal("request::wallpaper", function(s)
         awful.wallpaper {
@@ -57,11 +31,43 @@ local function wallpaper_solo(screen)
     end)
 end
 
+local function wallpaper_slideshow(screen)
+    local wallpapers = fs.get_files(envs.environment.wallpaper_dir)
+    local random_wallpaper = function()
+        return wallpapers[math.random(1, #wallpapers)]
+    end
+
+    if next(wallpapers) == nil then return end
+
+    setup_wallpaper(screen, random_wallpaper())
+
+    local wallpaper_timeout = envs.environment.wallpaper_timeout or 600
+    local wallpaper_timer = timer {timeout = wallpaper_timeout}
+
+    wallpaper_timer:connect_signal("timeout", function()
+        wallpaper_timer:stop() --       
+
+        change_wallpaper(screen, random_wallpaper())
+
+        wallpaper_timer.timeout = wallpaper_timeout
+        wallpaper_timer:start()
+    end)
+
+    wallpaper_timer:start()
+end
+
+local function wallpaper_solo(screen)
+    local wallpaper =
+        fs.resolve_path(envs.environment.wallpaper, envs.path_vars)
+
+    setup_wallpaper(wallpaper, screen)
+end
+
 function setup(screen)
     local slideshow_enabled = envs.environment.wallpaper_slideshow or "off"
 
     if slideshow_enabled == "on" then
-        wallpaper_slideshow(awful.screen.focused())
+        wallpaper_slideshow(screen)
     else
         wallpaper_solo(screen)
     end
