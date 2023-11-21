865a55a7f8b4fe0c13a076bd204dc684af19c8f3
Will <williampsena@gmail.com>
2023-11-21 18:20:23 -0300

    check slide show wallpaper

--- awesome/ebenezer/menu.lua
+++ awesome/ebenezer/menu.lua
@@ -3,6 +3,31 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 local envs = require('ebenezer.envs')
 local wallpaper = require('ebenezer.wallpaper')
 
+local function build_menu_items(beautiful)
+    local items = {
+        {"awesome", awesome_menu, beautiful.awesome_icon},
+        {"open terminal", terminal}
+    }
+
+    local slide_show_enabled = envs.environment.wallpaper_slideshow or "off"
+
+    if slide_show_enabled == "on" then
+        table.insert(items, {
+            "change wallpaper",
+            function()
+                wallpaper.change_random_wallpaper(awful.screen.focused())
+            end
+        })
+    end
+
+    table.insert(items, {
+        "lock screen",
+        function() awful.spawn.with_shell(envs.commands.lock_screen) end
+    })
+
+    return items
+end
+
 local function factory(beautiful)
     local awesome_menu = {
         {
@@ -20,19 +45,7 @@ local function factory(beautiful)
 
     local main_menu = awful.menu({
         theme = {width = 300},
-        items = {
-            {"awesome", awesome_menu, beautiful.awesome_icon},
-            {"open terminal", terminal}, {
-                "change wallpaper", function()
-                    wallpaper.change_random_wallpaper(awful.screen.focused())
-                end
-            }, {
-                "lock screen",
-                function()
-                    awful.spawn.with_shell(envs.commands.lock_screen)
-                end
-            }
-        }
+        items = build_menu_items(beautiful)
     })
 
     local menu_launcher = awful.widget.launcher({
