4fd2019b79c9d076a976c0bdec950ab3ce8e18be
William Sena <me@willsena.dev>
2024-11-03 13:23:37 -0300

    fix(py): current layout icon text

--- qtile/ebenezer/widgets/layout.py
+++ qtile/ebenezer/widgets/layout.py
@@ -1,4 +1,5 @@
-from libqtile import widget
+from libqtile import bar, hook
+from libqtile.widget import base
 
 from ebenezer.core.config.settings import AppSettings
 from ebenezer.widgets.helpers.args import build_widget_args
@@ -19,17 +20,46 @@ ICONS = {
 }
 
 
-class CurrentLayoutFontIcon(widget.CurrentLayout):
+class CurrentLayoutFontIcon(base._TextBox):
     """
     SDisplay the font icon related to the current layout of the current group of the screen,
     the bar containing the widget, is on.
     """
 
+    def __init__(self, width=bar.CALCULATED, **config):
+        base._TextBox.__init__(self, "", width, **config)
+
+    def _configure(self, qtile, bar):
+        base._TextBox._configure(self, qtile, bar)
+        layout_id = self.bar.screen.group.current_layout
+        self.text = self._fetch_icon(self.bar.screen.group.layouts[layout_id].name)
+        self.setup_hooks()
+
+        self.add_callbacks(
+            {
+                "Button1": qtile.next_layout,
+                "Button2": qtile.prev_layout,
+            }
+        )
+
     def hook_response(self, layout, group):
         if group.screen is not None and group.screen == self.bar.screen:
-            layout_icon = ICONS.get(layout.name, layout.name)
-            self.text = f"{layout_icon} "
+            self.text = self._fetch_icon(layout.name)
             self.bar.draw()
+
+    def setup_hooks(self):
+        hook.subscribe.layout_change(self.hook_response)
+
+    def remove_hooks(self):
+        hook.unsubscribe.layout_change(self.hook_response)
+
+    def finalize(self):
+        self.remove_hooks()
+        base._TextBox.finalize(self)
+
+    def _fetch_icon(self, layout_name: str):
+        layout_icon = ICONS.get(layout_name, layout_name)
+        return f"{layout_icon} "
 
 
 def build_current_layout_widget(settings: AppSettings, kwargs: dict):
