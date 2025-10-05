383d8145e14d43a45ecea960e64165605d4ac0ca
William Sena <me@willsena.dev>
2025-10-05 10:21:29 -0300

    feat(hypr): flameshot support

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,18 @@
 set fish_greeting
 
+if test -f $HOME/.profile
+    for line in (bash -c "source $HOME/.profile >/dev/null 2>&1; env" | grep -v '^BASH_FUNC_')
+        set -l name (echo $line | cut -d= -f1)
+        set -l value (echo $line | cut -d= -f2-)
+        
+        if contains $name PWD SHLVL _ 
+            continue
+        end
+        
+        set -x $name $value
+    end
+end
+
 if status is-interactive
     starship init fish | source
 end
